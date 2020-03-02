8bec5320117cccb7d3d08e435f1e97c4ea309790
ZACH CASLER <zcasler@uw.edu>
2020-03-02 12:38:46 -0800

    updated csv/summaryinfo

--- scripts/summary_info.R
+++ scripts/summary_info.R
@@ -1,4 +1,64 @@
 library("dplyr")
+library("tidyr")
+library("stringr")
 
 
-car_sales <- read.csv("")
+# following lines rearrange chart for better organization and access
+car_sales <- read.csv("../data/US_Vehicle_Sales_Monthly.csv",
+                      stringsAsFactors = FALSE)
+car_sales <- car_sales %>% gather(key = month, value = volume, -year)
+
+
+# following lines modify chart so data can be accessed easier
+us_reg_gas_prices <- read.csv("../data/U.S._Regular_Gasoline_Prices_Monthly.csv",
+                              stringsAsFactors = FALSE)
+us_reg_gas_prices <- us_reg_gas_prices[-c(182:354), ]
+us_reg_gas_prices <- us_reg_gas_prices %>% 
+  separate(date, sep = "-", into = c("year", "month"))
+us_reg_gas_prices$year <- str_pad(us_reg_gas_prices$year, 
+                                  width=2, 
+                                  side="left", 
+                                  pad="0")
+us_reg_gas_prices$year <- paste0(20, us_reg_gas_prices$year)
+
+
+
+get_df_summary1 <- function(df) {
+  output <- list()
+  output$num_columns <- length(df)
+  output$num_rows <- df %>% nrow()
+  output$start_year <- df %>% 
+    filter(year == min(year)) %>% 
+    pull(year) %>% 
+    unique()
+  output$end_year <- df %>%
+    filter(year == max(year)) %>% 
+    pull(year) %>% 
+    unique()
+  output$most_volume_ever <- df %>% 
+    filter(volume == max(volume)) %>% 
+    pull(volume)
+  output$most_volume_year <- df %>% 
+    filter(volume == max(volume)) %>% 
+    pull(year)
+  output$most_volume_month <- df %>% 
+    filter(volume == max(volume)) %>% 
+    pull(month)
+  output$num_best_year_volume <- car_sales %>% 
+    group_by(year) %>% 
+    summarize(total_volume = sum(volume)) %>% 
+    filter(total_volume == max(total_volume)) %>% 
+    pull(total_volume)
+  output$best_year_volume <- car_sales %>% 
+    group_by(year) %>% 
+    summarize(total_volume = sum(volume)) %>% 
+    filter(total_volume == max(total_volume)) %>% 
+    pull(year)
+  output
+} 
+
+test_summary <- get_df_summary1(car_sales)
+
+
+
+
