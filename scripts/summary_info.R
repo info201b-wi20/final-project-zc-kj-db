a23988d27ece596dd0e88a45cb20c592ea1e76b5
ZACH CASLER <zcasler@uw.edu>
2020-03-02 14:11:33 -0800

    updated csvs

--- scripts/summary_info.R
+++ scripts/summary_info.R
@@ -12,7 +12,6 @@ car_sales <- car_sales %>% gather(key = month, value = volume, -year)
 # following lines modify chart so data can be accessed easier
 us_reg_gas_prices <- read.csv("../data/U.S._Regular_Gasoline_Prices_Monthly.csv",
                               stringsAsFactors = FALSE)
-us_reg_gas_prices <- us_reg_gas_prices[-c(182:354), ]
 us_reg_gas_prices <- us_reg_gas_prices %>% 
   separate(date, sep = "-", into = c("year", "month"))
 us_reg_gas_prices$year <- str_pad(us_reg_gas_prices$year, 
@@ -56,9 +55,29 @@ get_df_summary1 <- function(df) {
     pull(year)
   output
 } 
+test_summary1 <- get_df_summary1(car_sales)
 
-test_summary <- get_df_summary1(car_sales)
-
-
-
-
+get_df_summary2 <- function(df) {
+  output <- list()
+  output$num_columns <- length(df)
+  output$num_rows <- df %>% nrow()
+  output$start_year <- df %>% 
+    filter(year == min(year)) %>% 
+    pull(year) %>% 
+    unique()
+  output$end_year <- df %>%
+    filter(year == max(year)) %>% 
+    pull(year) %>% 
+    unique()
+  output$num_highest_price <- df %>% 
+    filter(price == max(price)) %>% 
+    pull(price)
+  output$month_highest_price <- us_reg_gas_prices %>% 
+    filter(price == max(price)) %>% 
+    pull(month)
+  output$year_highest_price <- us_reg_gas_prices %>% 
+    filter(price == max(price)) %>% 
+    pull(year)
+  output
+}
+test_summary2 <- get_df_summary2(us_reg_gas_prices)
