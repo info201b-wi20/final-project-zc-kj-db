a315dfc4213d223c205a0a48d7552423c28d44c0
Kylie Jones <jonesk85@uw.edu>
2020-03-02 19:44:16 -0800

    styled files

--- scripts/chart2.R
+++ scripts/chart2.R
@@ -1,27 +1,27 @@
 library(plotly)
 
-get_mid_gas_chart <- function(){
-  us_mid_gas_prices <- read.csv("../data/U.S._Midgrade_Gasoline_Prices_Annual.csv",
-                                stringsAsFactors = FALSE)
-  
-  
+get_mid_gas_chart <- function() {
+
+  # Pull the data from our csv and convert it into a dataframe
+  us_mid_gas_prices <- read.csv(
+    "../data/U.S._Midgrade_Gasoline_Prices_Annual.csv",
+    stringsAsFactors = FALSE
+  )
+
+
   # Create a plot, then pipe that plot into the `layout()` function to modify it
-  # (Example adapted from the Plotly documentation)
   mid_chart <- plot_ly(
-    data = us_mid_gas_prices,      # pass in the data to be visualized
-    x = ~year, # use a formula to specify the column for the x-axis
-    y = ~price, # use a formula to specify the column for the y-axis
-    # use a formula to specify the color encoding
-    type = "scatter", # specify the type of plot to create
-    mode = "line"  # determine the "drawing mode" for the scatter (points)
+    data = us_mid_gas_prices, # data to be visualized
+    x = ~year, # x-axis
+    y = ~price, # y-axis
+    type = "scatter", # type of plot
+    mode = "line"
   ) %>%
     layout(
-      title = "Midgrade Gas Prices Through the Years",                  # plot title
+      title = "Midgrade Gas Prices Through the Years", # plot title
       xaxis = list(title = "year"), # axis label + format
-      yaxis = list(title = "Price", ticksuffix = "$")  # axis label + format
+      yaxis = list(title = "Price", ticksuffix = "$") # axis label + format
     )
-  
+
   return(mid_chart)
 }
-
-
